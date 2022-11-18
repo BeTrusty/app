97db64e5098e79ce3d617bf8c7b23296f0f15e7b
francocarballar <francocarballar@gmail.com>
2022-11-18 16:47:26 -0300

    Arregle el problema de inicio de sesión

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { SessionProvider } from 'next-auth/react'
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
 import { loggerLink } from '@trpc/client/links/loggerLink'
 import { withTRPC } from '@trpc/next'
-import { AppType } from 'next/dist/shared/lib/utils'
 import superjson from 'superjson'
 import { AppRouter } from '../server/router'
 import '../styles/globals.css'
@@ -13,6 +12,7 @@ import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
+import { AppProps } from 'next/app'
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
@@ -29,11 +29,11 @@ const wagmiClient = createClient({
   provider
 })
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <SessionProvider session={null}>
+        <SessionProvider session={pageProps.session}>
           <Provider>
             <>
               <Head>
--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { NavBar } from '@components/NavBar'
